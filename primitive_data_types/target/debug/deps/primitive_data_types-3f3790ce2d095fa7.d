715428a5f67c7deb4582b9c62fa6cf99e9d5d034
Mehedi Hasan <mehedi4958@gmail.com>
2026-09-10 23:03:27 +0600

    primitive data types

---
+++ primitive_data_types/target/debug/deps/primitive_data_types-3f3790ce2d095fa7.d
@@ -0,0 +1,5 @@
+/Users/mehedi/work/personal/rust-programming/primitive_data_types/target/debug/deps/primitive_data_types-3f3790ce2d095fa7.d: src/main.rs
+
+/Users/mehedi/work/personal/rust-programming/primitive_data_types/target/debug/deps/libprimitive_data_types-3f3790ce2d095fa7.rmeta: src/main.rs
+
+src/main.rs:
